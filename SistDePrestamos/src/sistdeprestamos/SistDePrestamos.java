f64b205feb94b1dc1b1e77cd495de86ca168424a
Samy Mahmod <symq9485@gmail.com>
2016-10-12 17:47:07 -0400

    Se agrego la clase archivo y se eliminaron los bucles de los try y catch

--- SistDePrestamos/src/sistdeprestamos/SistDePrestamos.java
+++ SistDePrestamos/src/sistdeprestamos/SistDePrestamos.java
@@ -19,35 +19,36 @@ class Solicitante
     String nombre; 
     String apellido1;
     String apellido2;
-    int telf;
-    int movil;
+    String telf;
+    String movil;
     
     
     void registrar() throws IOException 
     {
         BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        
         try
         {            
         System.out.print("Ingrese la cedula del solicitante: ");
         cedula = Integer.parseInt(br.readLine());
-        
+
         System.out.print("Ingrese el nombre del solicitante: ");
         nombre = br.readLine();
-        
+
         System.out.print("Ingrese el primer apellido del solicitante: ");
         apellido1 = br.readLine();
-        
+
         System.out.print("Ingrese el segundo apellido del solicitante: ");
         apellido2 = br.readLine();
-        
-        
+
+
         System.out.print("Ingrese el numero de telefono de habitacion del solicitante: ");
-        telf = Integer.parseInt(br.readLine());
-        
+        telf = br.readLine();
+
         System.out.print("Ingrese numero de telefono movil del solicitante: ");
-        movil = Integer.parseInt(br.readLine());
-    
+        movil = br.readLine();
         }
+
         catch(IOException e)
         {
             System.err.println("Error: " + e.getMessage());
@@ -105,37 +106,36 @@ class Prestamo {
 
         BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
 
-            try
+        try
+        {
+            System.out.print("Ingrese el valor del prestamo a solicitar");
+            while (siguiente == true && Valor_de_prestamo < Credito) 
             {
-                System.out.print("Ingrese el valor del prestamo a solicitar");
-                while (siguiente == true && Valor_de_prestamo < Credito) 
+                Credito=1000000.00d;
+                N_Prestamo = N_Prestamo + 1;
+                Valor_de_prestamo = Double.parseDouble(br.readLine());
+                Valor_de_prestamo = Valor_de_prestamo + Valor_de_prestamo;
+
+                if (Valor_de_prestamo >= Credito) 
                 {
-                    Credito=1000000.00d;
-                    N_Prestamo = N_Prestamo + 1;
-                    Valor_de_prestamo = Double.parseDouble(br.readLine());
-                    Valor_de_prestamo = Valor_de_prestamo + Valor_de_prestamo;
-                    
-                    if (Valor_de_prestamo >= Credito) 
-                    {
-                        System.out.println("Usted se ha excedido el limite");
-                        break;
-                    }
-                    
-                    System.out.println("Quiere hacer otro prestamo? S/N");
-                    
-                    seguir = br.readLine();
-                    
-                    if (seguir == "S")
-                    {
-                      siguiente = true;
-                    }
+                    System.out.println("Usted se ha excedido el limite");
+                    break;
                 }
 
+                System.out.println("Quiere hacer otro prestamo? S/N");
+
+                seguir = br.readLine();
+
+                if (seguir == "S")
+                {
+                  siguiente = true;
+                }
             }
-            catch(IOException e)
-            {
-                System.err.println("Error: " + e.getMessage());
-            }
+        }
+        catch(IOException e)
+        {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 
     public String capturar() 
@@ -144,13 +144,45 @@ class Prestamo {
     }
 }
 
+class Archivo
+{
+    PrintWriter pf;
+    FileReader fr;
+    
+    void crear(String pSolici, String pPresta) throws IOException
+    {
+        pf = new PrintWriter(new FileWriter(pSolici, true));
+        pf.println(pSolici);
+        pf.println(pPresta);
+        pf.close();
+        System.out.println("El archivo fue creado correctamente.");
+    }
+    
+    void leer (String pSolici) throws IOException
+    {
+        fr = new FileReader(pSolici);
+        BufferedReader br = new BufferedReader(fr);
+        String linea;
+        while((linea = br.readLine())!=null)
+        {
+            String[]datos=new String[4];
+            datos = linea.split("");
+            for(int i = 0; i <= datos.length-1;i++)
+            {
+                System.out.println(datos[i]);
+            }
+        }
+    }
+}
+
 public class SistDePrestamos 
 {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException 
     {
         // TODO code application logic here
     }
